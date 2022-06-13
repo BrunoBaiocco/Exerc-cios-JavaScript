f8a8b7d29f18b4e01efe1a090d6486bc5e37e96d
Bruno Baiocco <bruno.baiocco@plantae.agr.br>
2022-06-13 15:01:20 -0400

    1.1

---
+++ isBoolean/script/script.js
@@ -0,0 +1,15 @@
+// Faça uma função chamada isBoolean que recebe um 
+// parâmetro chamado val e retorna um boolean
+// indicando se o parâmetro é do tipo boolean;
+
+function isBoolean(val){
+    
+    // Retorna a verificação onde ve se o parâmetro val
+    //  é do tipo boolean, verificando se o valor e o tipo são iguais
+    return val === true || val === false ;
+}
+
+// console.log(isBoolean(true));
+// console.log(isBoolean(false));
+// console.log(isBoolean(1))
+// console.log(isBoolean(null)) 
---
+++ isNull/script/java.js
@@ -0,0 +1,14 @@
+// Faça uma função chamada isNull que recebe um
+// parâmetro chamado val e retorna um boolean
+// indicando se é nulo;
+
+function isNull(val){
+
+	//Retorna a verificação onde ve se o parametro val é igual a nulo 
+	return val == null;
+}
+
+// console.log(isNull(null));
+// console.log(isNull(0));
+// console.log(isNull(1));
+// console.log(isNull(''));
